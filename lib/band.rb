d970988d2f59fcae632c11228681d4df5ffd62d3
Sean Holstrom <basementbebop@gmail.com>
2015-01-30 14:38:25 -0800

    Add class validations

--- lib/band.rb
+++ lib/band.rb
@@ -1,6 +1,7 @@
 class Band < ActiveRecord::Base
   has_and_belongs_to_many :venues
   before_create(:capitalize_name)
+  validates(:name, :presence => true)
 
   private
   define_method(:capitalize_name) {
--- lib/venue.rb
+++ lib/venue.rb
@@ -1,6 +1,7 @@
 class Venue < ActiveRecord::Base
   has_and_belongs_to_many :bands
   before_create(:capitalize_name)
+  validates(:name, :presence => true)
 
   private
   define_method(:capitalize_name) {
